fa11a972de8293f97e25e23553c19ce05d118451
Ji Woong Park <jiwoongpark@jis-imac.dhcp.wustl.edu>
2018-03-31 15:39:07 -0500

    Uploading the final app for Udacity

---
+++ README.md
@@ -0,0 +1,1 @@
+# genetics101_android_app
---
+++ app/src/main/java/com/example/android/genetics101/MainActivity.java
@@ -0,0 +1,168 @@
+// Modified from Udacity's Just Java app
+
+        package com.example.android.genetics101;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.text.NumberFormat;
+
+/**
+ * This app displays an order form for taking genetics test.
+ */
+public class MainActivity extends AppCompatActivity {
+
+    int score = 0;
+
+    int correct_questions = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * This method is called when the answer button is clicked.
+     */
+    public void submitAnswer(View view) {
+        // Get user's name
+        EditText nameField = (EditText) findViewById(R.id.name_field);
+        Editable nameEditable = nameField.getText();
+        String name = nameEditable.toString();
+
+        // Checkboxes
+        CheckBox question1CheckBox = (CheckBox) findViewById(R.id.question1_checkbox);
+        boolean question1Correct = question1CheckBox.isChecked();
+
+        CheckBox question2CheckBox = (CheckBox) findViewById(R.id.question2_checkbox);
+        boolean question2Correct = question2CheckBox.isChecked();
+
+        CheckBox question3CheckBox = (CheckBox) findViewById(R.id.question3_checkbox);
+        boolean question3Correct = question3CheckBox.isChecked();
+
+        CheckBox question4CheckBox = (CheckBox) findViewById(R.id.question4_checkbox);
+        boolean question4Correct = question4CheckBox.isChecked();
+
+        CheckBox question5CheckBox = (CheckBox) findViewById(R.id.question5_checkbox);
+        boolean question5Correct = question5CheckBox.isChecked();
+
+        // Calculate the points
+        int point = calculatePrice(question1Correct, question2Correct, question3Correct, question4Correct, question5Correct);
+
+        // Display the summary on the screen
+        String message = createTestSummary(name, point, question1Correct, question2Correct, question3Correct, question4Correct, question5Correct);
+
+       displayMessage(message);
+
+       // Essential to make sure points don't go up just for clicking the questions.
+       score = 0;
+       correct_questions = 0;
+        }
+
+    /**
+     * Calculates the points.
+     *
+     * @param addQuestion1    is whether or not the first question is answered correct
+     * @param addQuestion2    is whether or not the second question is answered correct
+     * @param addQuestion3    is whether or not the third question is answered correct
+     * @param addQuestion4    is whether or not the fourth question is answered correct
+     * @param addQuestion5    is whether or not the fifth question is answered correct
+     * @return total point [score is the initial; point is what's finally returned]
+     */
+    private int calculatePrice(boolean addQuestion1, boolean addQuestion2, boolean addQuestion3, boolean addQuestion4, boolean addQuestion5) {
+
+        // Increments by one point
+        if (addQuestion1) {
+            correct_questions = correct_questions + 1;
+        }
+
+        if (addQuestion2) {
+            correct_questions = correct_questions + 1;
+        }
+
+        if (!addQuestion3) {
+            correct_questions = correct_questions + 1;
+        }
+
+        if (addQuestion4) {
+            correct_questions = correct_questions + 1;
+        }
+
+        if (!addQuestion5) {
+            correct_questions = correct_questions + 1;
+        }
+
+        // Calculate the total points; each question is worth 2 points
+        return score + correct_questions*2;
+    }
+
+    /**
+     * Create summary of the test.
+     *
+     * @param name            on the test
+     * @param addQuestion1    is whether or not to add chocolate to the coffee
+     * @return text summary
+     */
+    private String createTestSummary(String name, int point,
+                                      boolean addQuestion1, boolean addQuestion2, boolean addQuestion3, boolean addQuestion4, boolean addQuestion5) {
+        String testMessage = getString(R.string.test_summary_name, name);
+        testMessage += "\n" + getString(R.string.test_summary_question1, addQuestion1);
+        testMessage += "\n" + getString(R.string.test_summary_question2, addQuestion2);
+        testMessage += "\n" + getString(R.string.test_summary_question3, !addQuestion3);
+        testMessage += "\n" + getString(R.string.test_summary_question4, addQuestion4);
+        testMessage += "\n" + getString(R.string.test_summary_question5, !addQuestion5);
+        testMessage += "\n" + getString(R.string.test_summary_correct, correct_questions);
+        testMessage += "\n" + getString(R.string.test_summary_score, point);
+        testMessage += "\n" + getString(R.string.thank_you);
+        return testMessage;
+    }
+
+
+    private void displayMessage(String message) {
+        TextView testSummaryTextView = (TextView) findViewById(R.id.test_summary_text_view);
+        testSummaryTextView.setText(message);
+    }
+
+    // used when reset button is clicked
+    public void resetQuiz(View view) {
+
+        score = 0;
+        correct_questions = 0;
+
+
+        EditText nameField = (EditText) findViewById(R.id.name_field);
+        nameField.setText(null);
+        CheckBox question1CheckBox = (CheckBox) findViewById(R.id.question1_checkbox);
+        CheckBox question2CheckBox = (CheckBox) findViewById(R.id.question2_checkbox);
+        CheckBox question3CheckBox = (CheckBox) findViewById(R.id.question3_checkbox);
+        CheckBox question4CheckBox = (CheckBox) findViewById(R.id.question4_checkbox);
+        CheckBox question5CheckBox = (CheckBox) findViewById(R.id.question5_checkbox);
+
+        if (question1CheckBox.isChecked()) {
+            question1CheckBox.setChecked(false);
+        }
+        if (question2CheckBox.isChecked()) {
+            question2CheckBox.setChecked(false);
+        }
+        if (question3CheckBox.isChecked()) {
+            question3CheckBox.setChecked(false);
+        }
+        if (question4CheckBox.isChecked()) {
+            question4CheckBox.setChecked(false);
+        }
+        if (question5CheckBox.isChecked()) {
+            question5CheckBox.setChecked(false);
+        }
+
+            String message = "Your answers are reset. Try again. All checkboxes are unchecked now";
+        displayMessage(message);
+    }
+}
